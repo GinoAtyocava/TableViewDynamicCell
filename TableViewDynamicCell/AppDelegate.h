1ff39f3f3dea6d0b18bd87a9c71fd3e0840e53e3
GinoAtyocava <jingjing.deng@yocava.com>
2016-04-06 19:56:03 +0800

    Initial Commit

---
+++ TableViewDynamicCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewDynamicCell
+//
+//  Created by Gino on 16/4/6.
+//  Copyright © 2016年 Gino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewDynamicCell/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableViewDynamicCell
+//
+//  Created by Gino on 16/4/6.
+//  Copyright © 2016年 Gino. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
